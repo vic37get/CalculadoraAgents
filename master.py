3bc29fbdbb8f53975c18ac962913e2f30e2c7927
Victor Ribeiro da Silva <51518489+vic37get@users.noreply.github.com>
2022-12-14 13:33:02 -0300

    comentários

--- master.py
+++ master.py
@@ -12,61 +12,63 @@ def buscaParenteses(expressao):
         if caractere == ')':
             fechamento_parenteses.append(index)
     try:
+        #Pegar o parêntese mais interno, para isso o inicial é o mais á direita (da lista de abertura de parenteses) e o final o mais á esquerda (da lista de fechamento de parenteses).
         inicio = max(abertura_parenteses)
         fim = min(fechamento_parenteses)
-        #Se existem parenteses concorrentes. Exemplo: (2+5) - (5-7)
+        #Se existem parenteses concorrentes. Exemplo: (2+5) - (5-7), o parentese de inicio vai ter indice maior que o de fim.
         if inicio > fim:
             for caractere in range(fim, 0, -1):
                 if expressao[caractere] == '(':
                     inicio = caractere
                     return inicio, fim+1
-        return inicio, fim+1 #Retorna o inicio e o fim da expressao com parênteses incluindo os parênteses.
+        return inicio, fim+1 #Retorna o inicio e o fim da expressao com parênteses, incluindo os parênteses.
     except:
         return None, None
 
 #Função para identificar os números envolvidos na operação, pegando a partir do indice do simbolo da operação
 def identificaNumerais(expressao, indice_caractere):
+    #regex para identificar operações matemáticas
     OPERACOES = re.compile('(((raiz)|([\^*\/+\-])))')
+    #regex para identificar numeros, sejam inteiros ou decimais.
     NUMERO = re.compile('(\-?[0-9]{1,}(\.)?([0-9]{0,}))')
-    INICIO = ''
-    FIM = ''
 
+    PRIMEIRO_NUMERO = ''
+    SEGUNDO_NUMERO = ''
     #Buscando o número que fica antes do sinal da operação, ou seja, o primeiro número da operação.
     for caractere in range(indice_caractere-1, -1, -1):
         if re.search(OPERACOES, expressao[caractere]) == None:
             if expressao[caractere] != '(':
-                INICIO = str(expressao[caractere]) + INICIO
-                print(INICIO)
+                PRIMEIRO_NUMERO = str(expressao[caractere]) + PRIMEIRO_NUMERO
             else:
                 continue
         else:
             if expressao[caractere] == '-':
                 busca = expressao[:caractere]
                 if re.search(NUMERO, busca) == None:
-                    INICIO = str(expressao[caractere]) + INICIO
+                    PRIMEIRO_NUMERO = str(expressao[caractere]) + PRIMEIRO_NUMERO
             break
 
     #Buscando o número que fica depois do sinal da operação, ou seja, o segundo número da operação.
     for caractere in range(indice_caractere+1, len(expressao)):
         if re.search(OPERACOES, expressao[caractere]) == None:
             if expressao[caractere] != ')':
-                FIM += str(expressao[caractere])
+                SEGUNDO_NUMERO += str(expressao[caractere])
             else:
                 continue
         else:
             break
     #Condição em que se verifica se tem um número negativo mais á esquerda, se não tiver é null.
-    if INICIO == '':
-        return None, FIM
+    if PRIMEIRO_NUMERO == '':
+        return None, SEGUNDO_NUMERO
 
-    return INICIO, FIM
+    return PRIMEIRO_NUMERO, SEGUNDO_NUMERO
 
 #Função para verificar se ainda existe uma operação a ser resolvida na expressão.
 def temOperacao(expressao_testada):
+    #Regex que identifica numeros e operações (se existir uma operação obrigatoriamente deve existir numeros ao redor dela).
     OPERACOES = re.compile('([0-9]{1,}(\.)?([0-9]{0,})(((raiz)|([\^*\/+\-])))[0-9](\.)?([0-9]{0,}))')
     busca_operacoes = re.search(OPERACOES, expressao_testada)
     return busca_operacoes
-
 
 #Função para identificar a operação a ser realizada.
 def identificaOperacao(inicio_parenteses, fim_parenteses, expressao):
@@ -74,63 +76,87 @@ def identificaOperacao(inicio_parenteses, fim_parenteses, expressao):
     for operacao in lista_operacoes:
         for indice, caractere in enumerate(expressao[inicio_parenteses:fim_parenteses]):
             if caractere == operacao:
-                INICIO, FIM = identificaNumerais(expressao[inicio_parenteses:fim_parenteses], indice)
-                if INICIO == None:
+                PRIMEIRO_NUMERO, SEGUNDO_NUMERO = identificaNumerais(expressao[inicio_parenteses:fim_parenteses], indice)
+                #Se o primeiro numero é None, quer dizer que se trata de um número negativo (Por exemplo: -125, a operação é -, mas o primeiro numero é None.)
+                if PRIMEIRO_NUMERO == None:
                     continue
+                #Se for exponenciação
                 if operacao == '^':
-                    resultado = exponenciacao(INICIO, FIM)
-                    return '{}{}{}'.format(INICIO, operacao, FIM), resultado
+                    resultado = exponenciacao(PRIMEIRO_NUMERO, SEGUNDO_NUMERO)
+                    return '{}{}{}'.format(PRIMEIRO_NUMERO, operacao, SEGUNDO_NUMERO), resultado
 
+                #Se for raiz
                 elif operacao == 'raiz':
-                    resultado = raiz(INICIO, FIM)
-                    return '{}{}{}'.format(INICIO, operacao, FIM), resultado
+                    resultado = raiz(PRIMEIRO_NUMERO, SEGUNDO_NUMERO)
+                    return '{}{}{}'.format(PRIMEIRO_NUMERO, operacao, SEGUNDO_NUMERO), resultado
 
+                #Se for multiplicação
                 elif operacao == '*':
-                    resultado = multiplicacao(INICIO, FIM)
-                    return '{}{}{}'.format(INICIO, operacao, FIM), resultado
-                    
-                elif operacao == '/':
-                    resultado = divisao(INICIO, FIM)
-                    return '{}{}{}'.format(INICIO, operacao, FIM), resultado
+                    resultado = multiplicacao(PRIMEIRO_NUMERO, SEGUNDO_NUMERO)
+                    return '{}{}{}'.format(PRIMEIRO_NUMERO, operacao, SEGUNDO_NUMERO), resultado
 
+                #Se for divisão
+                elif operacao == '/':
+                    resultado = divisao(PRIMEIRO_NUMERO, SEGUNDO_NUMERO)
+                    return '{}{}{}'.format(PRIMEIRO_NUMERO, operacao, SEGUNDO_NUMERO), resultado
+
+                #Se for adição
                 elif operacao == '+':
-                    resultado = adicao(INICIO, FIM)
-                    return '{}{}{}'.format(INICIO, operacao, FIM), resultado
-                    
+                    resultado = adicao(PRIMEIRO_NUMERO, SEGUNDO_NUMERO)
+                    return '{}{}{}'.format(PRIMEIRO_NUMERO, operacao, SEGUNDO_NUMERO), resultado
+
+                #Se for subtração
                 elif operacao == '-':
-                    resultado = subtracao(INICIO, FIM)
-                    return '{}{}{}'.format(INICIO, operacao, FIM), resultado
+                    resultado = subtracao(PRIMEIRO_NUMERO, SEGUNDO_NUMERO)
+                    return '{}{}{}'.format(PRIMEIRO_NUMERO, operacao, SEGUNDO_NUMERO), resultado
     return None, None
 
 #Função para remover os parênteses.
 def removeParenteses(expressao):
+    #Regex para identificar parênteses
     PARENTESES = re.compile('[\)\()]')
     expressao = re.sub(PARENTESES, '', expressao)
     return expressao
             
 #Função que é o mestre, começa buscando as expressões entre parênteses. (precedencia)
 def Master(expressao):
+    #Se existe parenteses na expressão.
     parenteses = True
+    #Se a expressao ja está resolvida.
     resolvida = False
+    
     while parenteses == True:
         print(expressao)
+        #Encontra os parenteses
         inicio_parenteses, fim_parenteses = buscaParenteses(expressao)
         if inicio_parenteses != None or fim_parenteses != None:
             parenteses = True
+            #Verifica se tem operação dentro do parentese encontrado
             if temOperacao(expressao[inicio_parenteses:fim_parenteses]) != None:
+                #Se houver, identifica a operação, chama a função que calcula aquela expressao e retorna o resultado.
                 EXPRESSAO_RESOLVIDA, RESULTADO = identificaOperacao(inicio_parenteses, fim_parenteses, expressao)
+                #Após calcular, substitui na expressao original, a expressão resolvida pelo resultado dela.
                 expressao = expressao.replace(str(EXPRESSAO_RESOLVIDA), str(RESULTADO))
+            #Caso não exista operações a serem resolvidas dentro do parentese
             else:
+                #Remove os parenteses da expressao.
                 exp_sem_parenteses = removeParenteses(expressao[inicio_parenteses:fim_parenteses])
+                #Remove da expressao original os parenteses.
                 expressao = expressao.replace(expressao[inicio_parenteses:fim_parenteses], exp_sem_parenteses)
         else:
+            #A expressao original nao tem mais parenteses.
             parenteses = False
     
+    #Enquanto a expressao não estiver resolvida.
     while(resolvida == False):
+        #Verifica se existe operação
         if temOperacao(expressao[inicio_parenteses:fim_parenteses]) != None:
-                EXPRESSAO_RESOLVIDA, RESULTADO = identificaOperacao(inicio_parenteses, fim_parenteses, expressao)
-                expressao = expressao.replace(str(EXPRESSAO_RESOLVIDA), str(RESULTADO))
+            #Se houver, identifica a operação, chama a função que calcula aquela expressao e retorna o resultado.
+            EXPRESSAO_RESOLVIDA, RESULTADO = identificaOperacao(inicio_parenteses, fim_parenteses, expressao)
+            #Após calcular, substitui na expressao original, a expressão resolvida pelo resultado dela.
+            expressao = expressao.replace(str(EXPRESSAO_RESOLVIDA), str(RESULTADO))
         else:
+            #A expressão está resolvida.
             resolvida = True
 
 
